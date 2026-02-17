4d67c2fb0ef6c2d66fd80f0cbeb76d65b0e25b26
GabrielSan0 <lucas159san@gmail.com>
2026-02-16 21:17:50 -0500

    cambios

--- src/app/interfaces/becados.ts
+++ src/app/interfaces/becados.ts
@@ -8,7 +8,7 @@ export const BECADOS_DATA: AuthUser[] = [
   { username: 'eperez', name: 'Esmeralda Pérez Gómez', dni: '60338189' },
   { username: 'icosme', name: 'Isabel Yesenia Cosme Garcilazo', dni: '61088077' },
   { username: 'aflores', name: 'Anyeli Flores Pino', dni: '60774149' },
-  { username: 'jcastillo', name: 'Franklin Olortegui Dominguez', dni: '60022089' },
+  { username: 'folortegui', name: 'Franklin Olortegui Dominguez', dni: '60022089' },
   { username: 'yreyquez', name: 'Yeff Yayko Requez Flores', dni: '60623170' },
   { username: 'naldana', name: 'Nicol Sharlot Aldana Huaraca', dni: '61253853' },
   { username: 'jsaavedra', name: 'Jhordan Michael Saavedra Condori', dni: '61091722' },
@@ -20,7 +20,7 @@ export const BECADOS_DATA: AuthUser[] = [
   { username: 'frodriguez', name: 'Fernando Rodriguez Malvas', dni: '63301006' },
   { username: 'mcarhuas', name: 'Margarita Samar Carhuas', dni: '60567906' },
   { username: 'lchavez', name: 'Luz Yadira Chavez Ramirez', dni: '60228196' },
-  { username: 'jperez', name: 'Jairo Irven Perez Huiza', dni: '61276579' },
+  { username: 'jperezhuiza', name: 'Jairo Irven Perez Huiza', dni: '61276579' },
   { username: 'kluicho', name: 'Kristel Yassady Luicho Tarazona', dni: '61027487' },
   { username: 'cchavez', name: 'Caterin Diana Chavez Ferrer', dni: '61111296' },
 
